097fb03616fbc61e141636b705432e1f23daae97
Younes <oiyounes1@gmail.com>
2025-05-05 14:54:33 +0200

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() { 
+  const books = [ 
+      { 
+  title: "1984", author: "George Orwell" }, 
+      { 
+    ]; 
+  title: "Le Meilleur des Mondes", author: "Aldous Huxley" } 
+  return ( 
+  <div> 
+  <h1>Catalogue de livres</h1> 
+  <ul> 
+          {books.map((book, i) => ( 
+  <li key={i}>{book.title} - {book.author}</li> 
+          ))} 
+  </ul> 
+  </div> 
+    );  
